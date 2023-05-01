24b6e4b51328c29125b07b2a473f1c797cf071f7
Nykyfor Drahomaretskyi <thetridoknite@gmail.com>
2023-05-01 23:15:21 +0300

    init commit

---
+++ hm-1/main.js
@@ -0,0 +1,38 @@
+{
+    let a= 'hello';
+    let b = "owu";
+    let c = "com";
+    let d = "ua";
+    let e = 1;
+    let f = 10;
+    let g = -999;
+    let h = 123;
+    let i = 3.14;
+    let j = 2.7;
+    let k = 16;
+    console.log(a,b,c,d,e,f,g,h,i,j,k);
+}
+
+    let firstName = 'Nick'
+    let lastName = 'Dragomaretskyi'
+    let middleName = 'Victorovich'
+    let person = `${firstName} ${lastName} ${middleName}`
+
+    console.log(person);
+{
+    let a= 100;
+    console.log(typeof a);
+    let b = '100';
+    console.log(typeof b);
+    let c = true;
+    console.log(typeof c);
+}
+
+    let Name = prompt('Enter your name:')
+    console.log(firstName);
+
+    let surname = prompt('Enter your surname:')
+    console.log(lastName);
+
+    let middleName2 = prompt('Enter your middleName:')
+    console.log(middleName);
